aa4fb718b91fd9d5926b1fe488c2db744f449a75
Solnechnaya5 <60489280+Solnechnaya5@users.noreply.github.com>
2021-08-15 00:54:10 +0300

    Add files via upload

---
+++ js/w-height.js
@@ -0,0 +1,7 @@
+function setHeiHeight() {
+    $('#hei').css({
+        height: $(window).height() + 'px'
+    });
+}
+setHeiHeight(); // устанавливаем высоту окна при первой загрузке страницы
+$(window).resize( setHeiHeight ); // обновляем при изменении размеров окна
